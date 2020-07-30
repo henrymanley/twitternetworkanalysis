779fc6f47b57978d81a3dd4c0349c65d8a3fc70d
hmhacks <61797292+hmhacks@users.noreply.github.com>
2020-07-30 10:20:14 -0400

    Create sheet.R

---
+++ sheet.R
@@ -0,0 +1,16 @@
+#pulls from Gdrive. Need authentication. Downloads whole folder as list of sheets.
+data <- drive_get(id = "name of folder w all sheets", type = "spreadsheet")
+
+
+clist <- c("schoolnames")
+for (j in clist){
+  for (i in seq(1, length(data), 1)){
+    data[[i]]
+    file <- gs_read(ws = "name of the specific sheet")
+    file <- cbind(data[[i]], j)
+    file <- drive_publish(data[[i]]) 
+    file$published
+  }
+}
+
+googlesheets::gs_ws_ls(result)
